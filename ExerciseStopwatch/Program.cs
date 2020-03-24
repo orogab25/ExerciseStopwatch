d465a2ea50e989cb034c3dc3c8d90dff1d5398c9
orogab25 <orogab25@gmail.com>
2020-03-24 13:37:31 +0100

    Add project files.

---
+++ ExerciseStopwatch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExerciseStopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            /*stopwatch.Stop();*/
+            stopwatch.Start();
+            /*stopwatch.Start();*/
+            /*stopwatch.printDuration();*/
+            stopwatch.Stop();
+            stopwatch.printDuration();
+        }
+    }
+}
---
+++ ExerciseStopwatch/Stopwatch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseStopwatch
+{
+    class Stopwatch
+    {
+        private TimeSpan duration;
+        private DateTime tempStart;
+        private bool started = false;
+
+        public void Start()
+        {
+            if (started)
+            {
+                throw new InvalidOperationException("Stopwatch already started!");
+            }
+            else
+            {
+                tempStart = DateTime.Now;
+                started = true;
+            }
+        }
+
+        public void Stop()
+        {
+            if (!started)
+            {
+                throw new InvalidOperationException("First you need to start the stopwatch!");
+            }
+            else
+            {
+                duration = DateTime.Now - tempStart;
+                started = false;
+            }
+        }
+
+        public void printDuration()
+        {
+            if (!started)
+            {
+                int minutes = duration.Minutes;
+                int seconds = duration.Seconds;
+                int milliseconds = duration.Milliseconds;
+
+                Console.WriteLine("Duration: " + minutes + " minutes " + seconds + " seconds " + milliseconds+" milliseconds");
+            }
+            else
+            {
+                throw new InvalidOperationException("Stop the watch before printing out the duration!");
+            }
+        }
+    }
+}
